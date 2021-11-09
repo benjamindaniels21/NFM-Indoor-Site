4553e77fd77093e23950baab461b1711061eafd0
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-09 17:53:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Message from './Message';
+
+function App() {
+  const name = "Ben"
+  
+  return (
+    <div className="App">
+      <Message name= {19 + " " + name} />
+    </div>
+  );
+}
+
+export default App;
