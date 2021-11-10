b5ca2d58b4c6a3ea3c39951b13bb7d6dfaa5acd0
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-10 18:54:18 -0500

    working on nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
+import logo from '../assets/NFM-Logo.png'
+
 
 function Header(props) {
     return (
-        <nav className= "bg-red-400">
-            <div className="max-w-7xl mx-auto border border-red-400 bg-gray-500">
-                hello
+        <nav className="bg-black flex justify-between py-3">            
+        
+        {/* left side  */}
+            <div className="content-around bg-green-400 inline-grid " >
+                <h1 className="text-red-400 py-4 px-2 content-evenly">Home</h1>
             </div>
+            
+        {/* middle  */}
+            <div className="relative inline-grid w-20 h-20 cursor-pointer">
+                <img src={logo} alt="NFM Indoor Percussion"/>
+            </div>
+
+        {/* right side  */}
+            <div className="content-around bg-yellow-400 inline-grid " >
+                <h1 className="text-red-400 py-4 px-2 content-evenly">About</h1>
+            </div>
+
         </nav>
     )
 }
