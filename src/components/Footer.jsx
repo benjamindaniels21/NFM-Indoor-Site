49cd67cb41f860dc06fe41765366649fd1097764
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-12 20:03:54 -0500

    responsive hero img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(props) {
     return (
         <div className="bg-green-500 h-20">
-            <h1>The Footer</h1>
+            <h1 className="text-white bg-green-500">The Footer</h1>
         </div>
     )
 }
--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -2,8 +2,8 @@ import hero from '../assets/NFM4.JPG'
 
 export default function MainBody() {
     return (
-        <div>
-            <img src={hero} alt="drummer" />
+        <div className="">
+            <img className=" mx-auto max-h-full md:max-h-screen" src={hero} alt="drummer" />
         </div>
     )
 }
