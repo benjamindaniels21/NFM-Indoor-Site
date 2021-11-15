bd0617fa8d23a9cc7aabbeafd3c8cd256fe388d5
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-15 07:14:45 -0500

    instagram section added to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import "./index.css";
 import MainBody from "./components/MainBody";
 import Footer from "./components/Footer";
+import Instagram from "./components/Instagram";
 
 function App() {
   return (
     <div>
       <Header />
       <MainBody />
+      <Instagram />
       <Footer />
     </div>
   );
--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,7 +1,7 @@
 function Instagram() {
     return (
-        <div>
-            
+        <div className="bg-red-400 text-center h-48">
+            This is where the instagram portion will go. 
         </div>
     )
 }
