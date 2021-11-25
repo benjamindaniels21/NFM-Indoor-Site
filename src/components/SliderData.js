1f587c78aacd095bf618591f0ab671eeb02d2249
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-24 20:29:54 -0500

    working on cards for staff

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -2,16 +2,25 @@ export const SliderData = [
   {
     id: 0,
     image: "./photos/Charlie.jpg",
-    name: "Charlie",
+    name: "Charlie Lowe",
+    bio: "",
   },
   {
     id: 1,
     image: "./photos/Nick.jpg",
-    name: "Nick",
+    name: "Nick McConnaughey",
+    bio: "",
   },
   {
     id: 2,
     image: "./photos/Sami.jpg",
-    name: "Sami",
+    name: "Sami Maddox",
+    bio: "",
+  },
+  {
+    id: 3,
+    image: "",
+    name: "Rob Law",
+    bio: "",
   },
 ];
--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -2,20 +2,26 @@ import {SliderData} from "./SliderData";
 
 const Staff = () => {
 
-    return (
-        <>
-        <h1 className="text-white">Staff</h1>
-         {SliderData.map((slide) => {
-            return ( 
-            <div>
-                <h1 className="text-white">{slide.name}</h1>
-                <img src = {slide.image} alt={slide.name} height='200' width= '200'/>
-               
-            </div>
-            );
-        })}             
-        </>
+    return(
+        <div className="items-center text-center">   
+            <h1 className="text-white text-5xl text-center mb-4">Staff</h1>
+
+            {SliderData.map((slide) => {
+                return (
+                    <div class="flex flex-wrap max-w-sm rounded overflow-hidden shadow-lg bg-white mb-2">
+                        <img class="w-full h-auto" src={slide.image} alt="Sunset in the mountains" />
+                        <div class="px-6 py-4 bg-white">
+                            <div class="font-bold text-xl mb-2 bg-white">{slide.name}</div>
+                                <p class="text-gray-700 text-base bg-white">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                </p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div> 
     );
+
 }; 
 
 export default Staff;
