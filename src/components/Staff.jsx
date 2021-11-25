baa38868c249c99529ad5fc7f9d774feabb3d1e1
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-25 11:13:49 -0500

    cards inline

--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -8,7 +8,7 @@ const Staff = () => {
 
             {SliderData.map((slide) => {
                 return (
-                    <div class="flex flex-wrap max-w-sm rounded overflow-hidden shadow-lg bg-white mb-2">
+                    <div class="flex-wrap max-w-sm rounded overflow-hidden shadow-lg bg-white m-2 inline-block">
                         <img class="w-full h-auto" src={slide.image} alt="Sunset in the mountains" />
                         <div class="px-6 py-4 bg-white">
                             <div class="font-bold text-xl mb-2 bg-white">{slide.name}</div>
