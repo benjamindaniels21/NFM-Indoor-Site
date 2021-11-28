d2f1a80c05bcf11681bf519a80d14cadc9c25ca9
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-27 20:11:38 -0500

    add links to all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import logo from '../assets/NFM-Logo.png'
+import logo from '../assets/NFM-Logo.png';
+import {Link} from "react-router-dom";
 
 function Header(props) {
     return (
@@ -8,24 +9,34 @@ function Header(props) {
         {/* left side  */}
           <div> 
                 <div className="  inline-grid" >
+                    <Link to="/">
                     <h1 className="cursor-pointer hover:text-red-500 text-white ml-7 mt-10">Home</h1>
+                    </Link>
                 </div>
                 <div className=" inline-grid justify-center ml-7" >
+                    <Link to="/staff">
                     <h1 className="text-white cursor-pointer hover:text-red-500 ">Staff</h1>
+                    </Link>
                 </div>
             </div>
         {/* middle  */}
             <div className="inline-grid w-20 h-20 cursor-pointer  ">
+                <Link to="/">
                 <img src={logo} alt="NFM Indoor Percussion"/>
-            </div>
+                </Link>
+           </div>
 
         {/* right side  */}
               <div className=" "> 
                 <div className="  inline-grid justify-center mr-7 mt-10" >
+                    <Link to="/contact">
                     <h1 className="text-white cursor-pointer hover:text-red-500">Contact</h1>
+                    </Link>               
                 </div>
                 <div className=" inline-grid justify-center mr-7 " >
+                    <Link to="/about">
                     <h1 className="text-white cursor-pointer hover:text-red-500">About</h1>
+                    </Link>
                 </div>
                 
             </div>
