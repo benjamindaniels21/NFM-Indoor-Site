a40ce5ee81f27a30a41b3f15e571be8235dd0c97
Ben Daniels <benjamindaniels21@gmail.com>
2021-12-01 19:56:13 -0500

    "add modals"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react';
+import ReactModal from 'react-modal';
+
 
 function Contact() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return (
         <div>
             <h1 className="text-white">Contact Us Goes Here</h1>
+                <button onClick={() => setModalIsOpen(true)} className="bg-green-400 p-2">Open Modal</button>
+                <ReactModal isOpen={modalIsOpen}>
+                    <div className="bg-white">
+                    <h1>Modal Title</h1>
+                    <p>Modal Content</p>
+                    <div>
+                        <button onClick={() => setModalIsOpen(false)} className="bg-green-400 p-2">Close Modal</button>
+                    </div>
+                    </div>
+                </ReactModal> 
         </div>
     )
 }
--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -1,15 +1,31 @@
 import {SliderData} from "./SliderData";
 import "./hr.css";
+import React, {useState} from 'react';
+import ReactModal from 'react-modal';
 
-const Staff = () => {
 
+
+const Staff = (props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
     return(
-        <div className="items-center text-center">   
+ 
+        <div className="items-center text-center">  
+        
             <h1 className="text-white text-5xl text-center mb-4 font-serif">Our Instructional Staff</h1>
-
+            <ReactModal isOpen={modalIsOpen}>
+                <div className="bg-white">
+                <h1>{props.name}</h1>
+                <p>Modal Content</p>
+                <div>
+                    <button onClick={() => setModalIsOpen(false)} className="bg-green-400 p-2">Close Modal</button>
+                </div>
+                </div>
+            </ReactModal>  
             {SliderData.map((slide) => {
                 return (
-                    <div class="card transform transition duration-500 ease-in-out hover:scale-105 cursor-pointer flex-wrap max-w-sm rounded overflow-hidden  shadow-inner-lg bg-white m-2 inline-block">
+                    
+                    <div onClick={() => setModalIsOpen(true)} class="card transform transition duration-500 ease-in-out hover:scale-105 cursor-pointer flex-wrap max-w-sm rounded overflow-hidden  shadow-inner-lg bg-white m-2 inline-block">
+                
                         <img class="w-full h-auto" src={slide.image} alt="Sunset in the mountains" />
                         <div class="px-6 py-4 bg-white">
                             <div class="font-bold text-xl mb-2 bg-white">{slide.name}</div>
