0a25a2c00fce62db408ebb1d28cf7e9eb3c25b57
Ben Daniels <benjamindaniels21@gmail.com>
2021-12-13 16:58:02 -0500

    updated menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import logo from '../assets/NFM-Logo.png';
 import {Link} from "react-router-dom";
 import "./hamburger.css";
-//add comment here
 
 function Header(props) {
     return (
@@ -12,17 +11,17 @@ function Header(props) {
             <div className="hidden md:block"> 
                 <div className="  inline-grid" >
                     <Link to="/">
-                    <h1 className="cursor-pointer hover:text-red-500 text-white ml-7 mt-10 text-xl font-serif">Home</h1>
+                    <h1 className="cursor-pointer hover:text-red-500 text-white ml-28 mt-10 text-xl font-serif nav-link">Home</h1>
                     </Link>
                 </div>
                 <div className=" inline-grid justify-center ml-7" >
                     <Link to="/staff">
-                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif">Staff</h1>
+                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif nav-link">Staff</h1>
                     </Link>
                 </div>
             </div>
         {/* middle  */}
-            <div className="inline-grid w-20 h-20 cursor-pointer  ">
+            <div className="ml-8 md:ml-0 inline-grid w-20 h-20 cursor-pointer  ">
                 <Link to="/">
                 <img className="transform hover:scale-105 hover:rotate-12 duration-200" src={logo} alt="NFM Indoor Percussion"/>
                 </Link>
@@ -32,20 +31,17 @@ function Header(props) {
             <div className="hidden md:block"> 
                 <div className="  inline-grid justify-center mr-7 mt-10" >
                     <Link to="/contact">
-                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif">Contact</h1>
+                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif nav-link">Contact</h1>
                     </Link>               
                 </div>
-                <div className=" inline-grid justify-center mr-7 " >
+                <div className=" inline-grid justify-center mr-28 " >
                     <Link to="/about">
-                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif">About</h1>
+                    <h1 className="text-white cursor-pointer hover:text-red-500 text-xl font-serif nav-link">About</h1>
                     </Link>
                 </div>
                 
             </div>
-            
-        <div className="md:hidden hamburger my-auto mr-5">
-           
-        </div>
+    
         </nav>
                 
        
